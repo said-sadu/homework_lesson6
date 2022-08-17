de9aa85c59b5155831d627125e271666be4c5b57
said_sadu <sadullaev.1996@gmail.com>
2022-08-17 20:38:31 +0300

    внесен коментарий

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Введите элементы: ");
 
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);  // не помню как изменить ошибку с Null в 
+                                                                      // console readline, поэтому программа не всегда срабатывает
 int count = 0;
 
 for (int i = 0; i < arr.Length; i++)
