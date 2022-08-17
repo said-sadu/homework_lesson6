18adea176aa79e870680139bdf350ac19aa5b851
said_sadu <sadullaev.1996@gmail.com>
2022-08-17 20:01:25 +0300

    решена 1 задача

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите элементы: ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
