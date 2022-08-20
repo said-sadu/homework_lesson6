f317f288bd018ee13af6b151cf56e8a82ca5bf67
said_sadu <sadullaev.1996@gmail.com>
2022-08-20 19:23:11 +0300

    еще поправки

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите элементы через пробел: ");
 
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);  // не помню как изменить ошибку с Null в 
-                                                                      // console readline, поэтому программа не всегда срабатывает
+                                                                      // console readline
 int count = 0;
 
 for (int i = 0; i < arr.Length; i++)
